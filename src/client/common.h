f2cb5effb8560369e8f2c497cb71c04933174b24
Dirk Lehmann <develop@dj-l.de>
2020-03-21 03:09:48 +0100

    src/client/main.cpp: Added, prints "Hello World!" ...

---
+++ src/client/common.h
@@ -0,0 +1,30 @@
+/* `lib@home`, a distributed calculations framework
+ * Copyright (C) 2020  Dirk "YouDirk" Lehmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef COMMON_H__
+#define COMMON_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STRING_LEN                 256
+
+typedef char                       string_t[STRING_LEN];
+
+#endif /* COMMON_H__  */
