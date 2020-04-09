0cdc172a60035608625808dd9de4d92afd127892
Dirk Lehmann <develop@dj-l.de>
2020-04-09 10:08:59 +0200

    CI: Okay, implemented an own Docker Action.  Hopefully it works ...

---
+++ .github/actions/doxygen-environment/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# lib@home, framework to develop distributed calculations.
+# Copyright (C) 2020  Dirk "YouDirk" Lehmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+if [ -z $@ ]; then
+  echo "$0: No command to execute given!" >&2;
+  exit 1;
+fi
+
+$@
